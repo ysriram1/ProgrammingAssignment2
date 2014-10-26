48e9edc4e2efa0b0382a37fc0abae9da4a51b344
Sriram Yarlagadda <ysriram@umich.edu>
2014-10-25 19:35:50 -0500

    Proj 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function looks to see if there already is a matrix and/or inverse already
+## If not it returns NULL (for the get() and getinverse() subfunctions)
+## The set() and setinverse() subfuctions are used to store a new matrix or its inverse
+## The second function acutally calculates the inverse function. 
 
-## Write a short comment describing this function
+## This function stores the matrix and its inverse in memory.
+## Using the <<- operator, the function sets the value of the matrix and its inverse in the parent environment
+## When a new matrix (or inverse) is set using the set() (or setinverse) sub function, m is set to NULL
+## this ensures that a new inverse matrix is first calculated using the cacheSolve function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  
+  setinverse <- function(inverse) m <<- inverse
+  
+  getinverse <- function() m
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function finds the inverse of a given matrix, x.
+## First the function checks to see if there already is a cached matrix m.
+## It uses the solve() function to find the inverse.
+## The product of a matrix and its inverse should give back the the identity matrix
+## The diag() function gives out the Identity matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m = x$getinverse
+  
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data = x$get()
+  m <- solve(data, diag(nrow(amatrix$get())), ...)
+  x$setinverse(m)
+  m
 }
